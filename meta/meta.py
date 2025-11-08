53e6275749ba9111fa022afc5990f0557aaced18
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-11-08 19:36:23 +0100

    Added basic implementation for metadata handling

---
+++ meta/meta.py
@@ -0,0 +1,98 @@
+from pipeline.namespace import Namespace
+from pathlib import Path
+from pipeline.node import Node
+import json
+
+# TODO this can be majorly optimised if a dictionary based impl is used instead of lists
+
+META_DIR_PATH = Path("cex")
+META_FILE_PATH = META_DIR_PATH / "cex.json"
+
+class NodeMeta:
+    def __init__(self, hash: str, input_hashes: dict[str, str | None]) -> None:
+        self.hash = hash
+        self.input_hashes = input_hashes
+
+    @staticmethod
+    def get_input_keys(node: Node):
+        return node.function.__annotations__.keys()
+
+    def update_from(self, node: Node):
+        new_keys = NodeMeta.get_input_keys(node)
+
+        new_hashes = dict.fromkeys(NodeMeta.get_input_keys(node))
+
+        for key in new_keys:
+            if self.input_hashes.__contains__(key):
+                new_hashes[key] = self.input_hashes[key]
+
+        self.input_hashes = new_hashes                
+
+    @classmethod
+    def init_from(cls, node: Node):
+        return cls(node.get_persistent_hash(), dict.fromkeys(NodeMeta.get_input_keys(node)))
+
+class NamespaceMeta:
+    def __init__(self, name: str, nodes: list[NodeMeta]) -> None:
+        self.name = name
+        self.nodes = nodes
+    
+    def get_node_meta(self, hash: str) -> NodeMeta | None:
+        nodes = [ns for ns in self.nodes if ns.hash == hash]
+
+        if len(nodes) == 0:
+            return
+        
+        return nodes[0] 
+
+    def update_from(self, nodes: list[Node]):
+        new_nodes = []
+
+        for node in nodes:
+            current_meta = self.get_node_meta(node.get_persistent_hash())
+            
+            if current_meta is None:
+                new_nodes.append(NodeMeta.init_from(node))
+                continue
+
+            new_nodes.append(current_meta.update_from(node))
+
+        self.nodes = new_nodes
+
+    @classmethod
+    def init_from(cls, name: str, nodes: list[Node]):
+        return cls(name, [NodeMeta.init_from(node) for node in nodes])
+
+class CexMeta:
+    def __init__(self, namespaces: list[NamespaceMeta]):
+        self.namespaces = namespaces
+
+    def add_namespace(self, namespace: Namespace, nodes: list[Node]):
+        self.namespaces.append(NamespaceMeta.init_from(namespace.name, nodes))
+
+    # TODO handle duplicate namespaces
+    def get_namespace(self, name: str) -> NamespaceMeta | None:
+        namespace_metas = [ns for ns in self.namespaces if ns.name == name]
+
+        if len(namespace_metas) == 0:
+            return
+        return namespace_metas[0]
+
+    @classmethod
+    def init_from(cls, namespaces: list[tuple[Namespace, list[Node]]]):
+        return cls([NamespaceMeta.init_from(namespace[0].name, namespace[1]) for namespace in namespaces])
+
+class ExecutionMetadataHandler:
+    def __init__(self) -> None:
+        META_DIR_PATH.mkdir(exist_ok=True)
+
+        if META_FILE_PATH.is_file():
+            with open(META_FILE_PATH, 'r', encoding='utf-8') as data:
+                self.data = CexMeta([NamespaceMeta(**ns) for ns in json.load(data)])
+
+        else:
+            self.data = CexMeta.init_from([])
+
+    def sync(self) -> None:
+        with open(META_FILE_PATH, 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, indent=4)
