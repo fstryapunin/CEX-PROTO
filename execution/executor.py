25c8408c4317c8440d83c7fd57b74f7e89c39647
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-11-08 23:15:14 +0100

    Added basic node state preparation

--- execution/executor.py
+++ execution/executor.py
@@ -4,19 +4,22 @@ import hashlib
 import inspect
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, get_origin
 import uuid
 from venv import logger
 
 
+from data.serializers import DataSerializer
+from meta.meta import ExecutionMetadataHandler
 from pipeline.namespace import Namespace
-from pipeline.node import Node
+from pipeline.node import Node, NodeState
 import networkx as nx
 import logging
 
 import itertools
 
-def get_file_hash(file_path, chunk_size=8192) -> str | None:
+
+def get_file_hash(file_path: Path, chunk_size=8192) -> str | None:
     if not os.path.exists(file_path):
         return None
 
@@ -28,10 +31,27 @@ def get_file_hash(file_path, chunk_size=8192) -> str | None:
     return hash_func.hexdigest()
 
 class NamespaceExecutor:
-    def __init__(self, namespace: Namespace) -> None:
+    def __init__(self, namespace: Namespace, meta_handler: ExecutionMetadataHandler) -> None:
         self.namespace = namespace
-        self.nodes_by_id: dict[uuid.UUID, Node] = dict()
-        self.input_paths: dict[uuid.UUID, dict[str, str]] = dict()
+        self.meta_handler = meta_handler
+        self.node_execution_states: dict[uuid.UUID, NodeState] = dict()
+        self.serializers: dict[type, DataSerializer] = dict()
+        self.default_serializer = DataSerializer()
+
+    # needed later for proper namespacing
+    #region Path handling
+
+    def resolve_output_directory_path(self, output_dir: str) -> Path:
+        return Path(output_dir)
+
+    def resolve_input_directory_path(self, input_dir: str) -> Path:
+        return Path(input_dir)
+    
+    # TODO implement after serialization
+    def resolve_output_path(self, node: Node, output_name: str, output_type: type) -> Path:
+        raise
+
+    #endregion
 
     def build_graph(self):
         graph = nx.DiGraph()
@@ -53,12 +73,13 @@ class NamespaceExecutor:
         
         return graph
 
-    def get_available_io_inputs(self, path: Path) -> list[str]:
-        stems = []
+    def get_available_io_inputs(self, input_dir: str) -> list[Path]:
+        path = self.resolve_input_directory_path(input_dir)
+        stems: list[Path] = []
         try:
             for entry in path.iterdir():
                 if entry.is_file():
-                    stems.append(entry.stem)
+                    stems.append(entry)
         
         except FileNotFoundError:
             logger.error(f"The directory '{path}' was not found.")
@@ -71,6 +92,8 @@ class NamespaceExecutor:
             return []
 
         return stems
+    
+    #region Validation
     
     def validate_node_parameters(self):
         result = True
@@ -104,8 +127,7 @@ class NamespaceExecutor:
                     logging.error(f"Invalid output configuration for node: {node.name}, duplicate output names are present.")
 
         return result
-
-                
+      
     def validate_input_dependencies(self):
         sorted_graph = nx.topological_sort(self.graph)
         available_inputs: dict[uuid.UUID, list[tuple[str, Any]]] = defaultdict()
@@ -117,14 +139,18 @@ class NamespaceExecutor:
                 result = False
                 continue
 
-            if node.input_directory is not None:
-                for input in self.get_available_io_inputs(node.input_directory):
-                    available_inputs[node.runtime_id].append((input, object))
+            if node.input_directory_name is not None:
+                for input in self.get_available_io_inputs(node.input_directory_name):
+                    available_inputs[node.runtime_id].append((input.stem, object))
             
             node_available_inputs = available_inputs.pop(node.runtime_id)
 
             for name, type in node.get_required_inputs():
                 aliases = node.input_aliases[name] if node.input_aliases is not None else [name]
+
+                if isinstance(aliases, str):
+                    aliases = [aliases]
+
                 matching_inputs = list(filter(lambda input: any([alias == input[0] for alias in aliases]) and issubclass(input[1], type), node_available_inputs))
                 
                 if len(matching_inputs) == 0:
@@ -141,15 +167,6 @@ class NamespaceExecutor:
                         available_inputs[subsequent_node.runtime_id].extend(outputs)
 
         return result
-    
-    def prepare_node_states(self):
-        pass
-
-    def load_meta(self):
-        pass
-
-    def update_meta(self):
-        pass
 
     def validate(self) -> bool:
         if not nx.is_directed_acyclic_graph(self.graph):
@@ -167,8 +184,99 @@ class NamespaceExecutor:
         logging.info("Succesfuly validated pipeline")
 
         return True
+    
+    #endregion
+    
+    #region Meta
+    
+    def load_meta(self):
+        nodes = list(self.graph)
+        stored = self.meta_handler.data.get_namespace(self.namespace.name)
+
+        if stored is None:
+            self.meta_data = self.meta_handler.data.create_namespace(self.namespace, nodes)
+            self.meta_handler.sync()
+            return
+        
+        stored.update_from(nodes)
+        self.meta_data = stored
+        self.meta_handler.sync()
+
+
+    def update_meta(self):
+        self.meta_handler.sync()
+
+    #endregion
+
+    def prepare_node_states(self):
+        sorted_graph = nx.topological_sort(self.graph)
+        available_input_hashes: dict[uuid.UUID, list[tuple[str, str | None]]] = defaultdict()
+        
+        for node in sorted_graph:
+            if not isinstance(node, Node):
+                logging.error(f"Invalid type error, {node} is not an instance of Node")
+                return
+            
+            if not node.is_cached:
+                self.node_execution_states[node.runtime_id] = NodeState.READY
+                continue
+
+            meta = self.meta_data.get_node_meta(node.get_persistent_hash())
+
+            if meta is None:
+                logger.fatal(f"Metadata not found for node {node.name}")
+                raise
+
+            # resolve available inputs and generate hashes
+            node_inputs = available_input_hashes.pop(node.runtime_id)
+            io_inputs = self.get_available_io_inputs(node.input_directory_name) if node.input_directory_name is not None else []
+
+            required_inputs = node.get_required_inputs()
+
+            input_hashes: dict[str, str | None] = dict()
+
+            for input in required_inputs:
+                names = node.get_inputs_aliases(input[0])
+                available_inputs: list[Path | tuple[str, str | None]] = []
+
+                for name in names:
+                    available_inputs += filter(lambda node_input: node_input[0] == name, node_inputs)
+                    available_inputs += filter(lambda node_input: node_input.stem == name, io_inputs)
+                
+                if len(available_inputs) > 1:
+                    logger.fatal(f"Ambiguous inputs for node {node.name}, function input {input}, aliases: {names}")
+                    raise
+                
+                if len(available_inputs) == 0:
+                    input_hashes[input[0]] = None
+                    continue
+
+                available_input = available_inputs[0]
+                
+                if isinstance(available_input, Path):
+                    input_hashes[input[0]] = get_file_hash(available_input)
+                else:
+                    input_hashes[input[0]] = available_input[1]
+
+            is_hashes_current = meta.is_current(input_hashes)
+
+            if not is_hashes_current:
+                self.node_execution_states[node.runtime_id] = NodeState.READY
+
+            if node.is_cached:
+                outputs = node.get_available_outputs()
+                if outputs is None:
+                    continue
+
+                outputs_with_hashes = [(output[0], get_file_hash(self.resolve_output_path(node, output[0], output[1]))) for output in outputs]
+
+                for subsequent_node in node.subsequent_nodes:
+                    available_input_hashes[subsequent_node.runtime_id].extend(outputs_with_hashes)
+
+            
 
     def prepare_execution(self):
         self.graph = self.build_graph()
         self.validate()
         self.load_meta()
+        self.prepare_node_states()
