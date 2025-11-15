49f5a4e46a200ddb36b1059c8abd12a7192acfd2
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-11-15 01:20:07 +0100

    Added default serializers

--- data/serializers.py
+++ data/serializers.py
@@ -1,12 +1,20 @@
 from abc import abstractmethod
+import csv
 import json
 from pathlib import Path
 from typing import Protocol, runtime_checkable
+
+import yaml
+
+from execution.common import RuntimeException
 
 @runtime_checkable
 class DataSerializer[TData](Protocol):
     @abstractmethod
     def get_file_extension(self) -> str:
+        raise NotImplementedError
+    @abstractmethod
+    def matches_file(self, extension: str):
         raise NotImplementedError
     @abstractmethod
     def load(self, path: Path) -> TData:
@@ -15,28 +23,93 @@ class DataSerializer[TData](Protocol):
     def save(self, path: Path, data: TData):
         raise NotImplementedError
 
-class DefaultSerializer:
+class CsvSerializer:
     def get_file_extension(self) -> str:
-        raise
-
-    def load(self, path):
-        raise
+        return ".csv"
     
-    def save(self, path, data):
-        raise
+    def matches_file(self, extension: str):
+        return extension == ".csv"
+
+    def load(self, path: Path):
+        if not path.is_file():
+            raise RuntimeException(f"File not found at {path}")
+        
+        data_list = []
+        with open(path, 'r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data_list.append(dict(row))
+        return data_list
+    
+    def save(self, path: Path, data):
+        path.parent.mkdir(exist_ok=True, parents=True)
+        fieldnames = list(data[0].keys())
+        
+        with open(path, 'w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader() 
+            writer.writerows(data)
+
+class PlainFileSerializer:
+    def get_file_extension(self) -> str:
+        return ".txt"
+
+    def matches_file(self, extension: str):
+        return extension == "txt"
+    
+    def load(self, path: Path) -> str:
+        if not path.is_file():
+            raise RuntimeException(f"File not found at {path}")
+            
+        with open(path, 'r', encoding='utf-8') as file:
+            return file.read()
+    
+    def save(self, path: Path, data):
+        path.parent.mkdir(exist_ok=True, parents=True)
+        
+        if isinstance(data, str):
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(data)
+        elif isinstance(data, bytes):
+            with open(path, 'wb') as file:
+                file.write(data)
+        else:
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(str(data))
+
 
 class JsonSerializer:
     def get_file_extension(self) -> str:
         return ".json"
     
+    def matches_file(self, extension: str):
+        return extension == "json"
+    
     def load(self, path):
         if path.is_file():
             with open(path, 'r', encoding='utf-8') as data:
                 return json.load(data)
-        else: raise Exception(f"File not found at {path}")
+        else: raise RuntimeException(f"File not found at {path}")
     
     def save(self, path: Path, data):
         path.parent.mkdir(exist_ok=True, parents=True)
         with open(path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4)
+
+class YamlSerializer:
+    def get_file_extension(self) -> str:
+        return ".yaml"
+    
+    def matches_file(self, extension: str):
+        return extension == "yml" or extension == "yaml"
+
+    def load(self, path):
+        if path.is_file():
+            with open(path, "r") as f:
+                return yaml.safe_load(f)
+        else: raise RuntimeException(f"File not found at {path}")
+    
+    def save(self, path: Path, data):
+        with open(path, "w") as f:
+            return yaml.safe_dump(data, f)
 
