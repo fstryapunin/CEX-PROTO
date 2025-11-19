f4c773477c26797632826f1eb4b58b3a2a7268b4
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-11-19 17:15:05 +0100

    Fixed serializers example

--- examples/serializers.py
+++ examples/serializers.py
@@ -6,6 +6,7 @@
 # A set of default serializers is provided at cex level to load file inputs by extension, if no serializer was given.
 # Provided serializers must implement DataSerializer protocol
 
+import json
 from pathlib import Path
 from data.serializers import JsonSerializer
 from pipeline.namespace import Namespace
@@ -21,8 +22,6 @@ if __name__ == "__main__":
     # Use default input serializer for json
     default_namespace = Namespace("DefaultJsonNamespace", Path("./examples/data"))
 
-
-
     json_node = Node(print_data, name="DataPrinter", input_directory="input", is_cached=False)
 
     default_namespace.add_root_node(json_node)
@@ -30,7 +29,7 @@ if __name__ == "__main__":
 
     # Specify serializer for input
 
-    specified_namespace = Namespace("DefaultJsonNamespace", Path("./examples/data"))
+    specified_namespace = Namespace("SpecifiedNamespace", Path("./examples/data"))
 
     specified_node = Node(print_data, name="DataPrinter", input_directory="input", is_cached=False, input_serializers=JsonSerializer())
 
@@ -43,20 +42,31 @@ if __name__ == "__main__":
     class MyData:
         def  __init__(self, name) -> None:
             self.name = name
+        
+        def to_serializable(self) -> dict:
+            return { 'Name' : self.name }
+
+        @classmethod
+        def from_dict(cls, dict: dict):
+            return cls(dict.get("Name"))
 
     class MySerializer:
-        DATA: MyData
         def get_file_extension(self) -> str:
-            return ".pkl"
+            return ".json"
         
         def matches_file(self, extension: str):
-            return extension == ".pkl" or extension == ".pickle"
+            return extension == ".json"
         
-        def load(self, path: Path):
-            return MySerializer.DATA
+        def load(self, path: Path) -> MyData:
+            if path.is_file():
+                with open(path, 'r', encoding='utf-8') as data:
+                    return MyData.from_dict(json.load(data))
+            else: raise Exception(f"File not found at {path}")
         
-        def save(self, path: Path, data):
-            MySerializer.DATA = data
+        def save(self, path: Path, data: MyData):
+            path.parent.mkdir(exist_ok=True, parents=True)
+            with open(path, 'w', encoding='utf-8') as file:
+                json.dump(data.to_serializable(), file, indent=4)            
 
     def create_my_data() -> MyData:
         return MyData("My name")
@@ -64,13 +74,13 @@ if __name__ == "__main__":
     def print_my_data(data: MyData):
         print(data.name)
 
-    my_data_namespace = Namespace("MyDataNamespace")
+    my_data_namespace = Namespace("MyDataNamespace", Path("./examples/data"))
     my_data_namespace.add_serializer_by_type(MyData, MySerializer())
     
-    create_my_data_node = Node(create_my_data, name="MyDataCreator", output_name="MyData",  is_cached=False)
+    create_my_data_node = Node(create_my_data, name="MyDataCreator", output_name="MyData")
     print_my_data_node = Node(print_my_data, name="MyDataPrinter", is_cached=False)
 
     create_my_data_node.continue_with(print_my_data_node)
 
     my_data_namespace.add_root_node(create_my_data_node)
-    # my_data_namespace.run()
+    my_data_namespace.run()
